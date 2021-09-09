6b1639127bc7b1532c1314658cff01e9b548c06c
JiangHuYiXiao <1163270704@qq.com>
2021-09-09 09:17:26 +0800

    day1作业

---
+++ 01 python基础/day01_pycharm的基本使用/homework/深圳-江湖-44.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2021/9/8 19:32
+# @Software       : PythonFullStack
+# @Python_verison : 3.7
+
+# 题目：下面那些不能作为变量？
+# 1、find    可以
+# 2、 num    不可以
+# 3、7val    不可以
+# 4、add.    不可以
+# 5、def     ****不可以****
+# 6、pan     可以
+# 7、-print  不可以
+# 8、open_file   可以
+# 9、FileName    可以
+# 10、9prints    不可以
+# 11、INPUT      可以
+# 12、ls         可以
+# 13、user^name  不可以
+# 14、list1      可以
+# 15、str        可以
+# 16、_888       可以
+# 17、is         可以
+# 18、true       不可以
+# 19、none       不可以
+# 20、try        不可以
+
+
+# 题目：变量的命名规范有哪些？
+    # 以字母、数字、下划线组成
+    # 不能以数字开头
+    # 不能是关键字
+    # 变量名称尽量做到见名知意
+
+# 题目：python 如何添加注释？
+# ctrl + /   单行注释
+'''
+多行注释
+'''
+INPUT = 'sdfsf'
+print(INPUT)
