ec1444fcb9272c45330bebe9748d7ec7af43dd09
JiangHuYiXiao <1163270704@qq.com>
2021-09-09 09:24:11 +0800

    day1笔记

--- 01 python基础/day01_pycharm的基本使用/notes.py
+++ 01 python基础/day01_pycharm的基本使用/notes.py
@@ -3,3 +3,16 @@
 # @Time           : 2021/9/8 19:23
 # @Software       : PythonFullStack
 # @Python_verison : 3.7
+
+# pycharm的基本使用：
+    # 创建项目
+    # 打开项目
+    # VCS项目，比如从git上clone项目
+    # 解释器，建议使用本地的python，不要创建虚拟环境，否则产生一些无法预置的错误
+    # 运行
+    # setting，主题、字体、文件模板设置，
+
+# python基本语法
+    # 变量
+
+    # 数据类型
