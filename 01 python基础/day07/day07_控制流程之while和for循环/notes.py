d66e0216e51fc2435de7c95bc8d7c7f71dd60cdb
JiangHuYiXiao <1163270704@qq.com>
2021-09-10 10:29:43 +0800

    补充enumerate，同时获取索引和key

--- 01 python基础/day07/day07_控制流程之while和for循环/notes.py
+++ 01 python基础/day07/day07_控制流程之while和for循环/notes.py
@@ -9,14 +9,30 @@
 #         改变条件语句
 
 # 2、while True：
-    # 死循环
+#     死循环
 
 
 # 3、for 循环结构：
 # for 条件：
 #     循环体
 
-# 4、break终止整个循环
+# enumumrate 同时获取索引和key
+for key,value in enumerate({'a':1,'b':2}):
+    print(key,value)    # 结果字典的索引，和key
+    # 0 a
+    # 1 b
+
+for key,value in {'a':1,'b':2}.items():
+    print(key,value)    # 字典的索引和值一般这样获取
+
+for key,value in enumerate([1,2,'balala']):
+    print(key,value)   # 结果为列表的索引和值
+# 0 1
+# 1 2
+# 2 balala
+
+# 4、break和continue
+# break终止整个循环
 # continue终止当前循环，然后执行下一次循环
 
 # 5、range
@@ -26,10 +42,10 @@ for i in range(10):
     print(i)
 
 # 6、print
-# print(end='\t')表示一行输出，默认是换行的\
+print(end='\t') # 表示一行输出，默认是换行的\
 
 # 7、格式化输出
-# 格式化输出format的另一种使用方式：f
+# 格式化输出的另一种使用方式：f
 a = 'jianghu'
 b = 19
 print(f'我的名字是{a}，我的年龄是{b}岁')
