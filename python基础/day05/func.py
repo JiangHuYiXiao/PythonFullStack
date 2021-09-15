4d5dbb44662ba0a1a505a86efa703c66ff9f1e12
JiangHuYiXiao <1163270704@qq.com>
2021-09-15 15:56:43 +0800

    day05

--- python基础/day05/func.py
+++ python基础/day05/func.py
@@ -3,5 +3,16 @@
 # @Time           : 2021/9/15 15:07
 # @Software       : PythonFullStack
 # @Python_verison : 3.7
+
+time1 = '2021-12-31'
+
 def test():
-    return None
+    return '测试一下导入函数'
+
+class Student:
+    def __init__(self,name,age):
+        name = '姚明'
+        age = age
+
+    def small_student(self):
+        return ('小学生')
