969714bc972b482b7a9a5c4beebbf67b1feeb6f7
JiangHuYiXiao <1163270704@qq.com>
2021-09-15 15:58:34 +0800

    模块和包的导入

---
+++ python基础/day09/包和模块/func2.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2021/9/15 15:52
+# @Software       : PythonFullStack
+# @Python_verison : 3.7
+def f():
+
+    return 'oooooooo'
--- python基础/day09/包和模块/module_demo.py
+++ python基础/day09/包和模块/module_demo.py
@@ -4,8 +4,8 @@
 # @Software       : PythonFullStack
 # @Python_verison : 3.7
 
-# 模块：所有的.py文件就可以称为模块，
-# 包：package，包含__init__.py文件，都可以称为包
+# 模块：所有的.py文件，就可以称为模块。
+# 包：package，包含__init__.py文件夹，都可以称为包。
 
 
 # 1、包和模块的导入
@@ -23,8 +23,46 @@ print(sys.path)
 #  'C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages',
 #  'F:\\PyCharm 2018.2.2\\helpers\\pycharm_matplotlib_backend']
 
-# 2、
-# from python基础.day05 import func.py
+# 2、from 包名 import 模块名
+# 包名是需要先看下有哪些路径添加到了sys.path下，然后再已经有的路径基础下进行导入
+# 'F:\\PythonFullStack',所以从这个路径下开始导入，也可以从项目根目录开始导入
+
+from python基础.day05 import func
+print(func.test())
+
+
+
+# 3、from 包名.模块名 import 函数名、类名、方法名、变量
+
+# 导入函数
+from python基础.day05.func import test
+print(test())
+
+
+# 导入变量
+from python基础.day05.func import time1
+print(time1)
+
+
+# 导入类
+from python基础.day05.func import Student
+print(Student)
+
+# 当本文件有相同的变量、函数、类、方法时，使用别名进行导入
+def test():
+    return 'hhhhh'
+from python基础.day05.func import test as test_one
+print(test())               # hhhhh
+print(test_one())
+
+
+# 如果导入的文件和sys.path中路径一样，则不需要from
+import func2
+
+print(func2.f())
+
+
+
 
 
 
