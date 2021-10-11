0af0f0651b45c6d2368d39637c39f4054f9dc29a
JiangHuYiXiao <1163270704@qq.com>
2021-10-11 12:50:25 +0800

    主动抛出异常后，后面的代码也不会继续执行了

--- python基础/day10/异常处理/主动抛出异常.py
+++ python基础/day10/异常处理/主动抛出异常.py
@@ -8,6 +8,8 @@ list = [1,2,4,'jianghu']
 j = 100
 if j > len(list)-1:
     raise IndexError('索引越界')
+    # 主动抛出异常后，后面的代码也不会继续执行了
+    print('hhhhhhh')
 print(list[j])
 
 #     raise IndexError('索引越界')
