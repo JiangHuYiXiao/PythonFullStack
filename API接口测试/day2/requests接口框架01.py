78b1c3db9587708673d73dbc64e8c2cb43c1e62d
JiangHuYiXiao <1163270704@qq.com>
2021-10-18 11:21:43 +0800

    更新run

--- API接口测试/day2/requests接口框架01.py
+++ API接口测试/day2/requests接口框架01.py
@@ -8,11 +8,13 @@
 import requests
 
 res = requests.get('https://www.baidu.com/')
+# res.encoding = 'utf-8'
 print(res)    # 返回一个对象<Response [200]>
 
 print(res.text)             #  返回一个字符串的数据类型，存储网页信息
 print(res.content)             # 返回一个byte数据类型，存储网页信息
-print(res.json())               # 返回一个字典的数据类型，存储的网页信息
+print(res.encoding)
+# print(res.json())               # 返回一个字典的数据类型，存储的网页信息
 print(res.status_code)              # 200
 
 
