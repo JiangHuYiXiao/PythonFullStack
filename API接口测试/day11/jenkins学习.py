e720c94d8e1681fed45d48e68fd1b71715ac89eb
JiangHuYiXiao <1163270704@qq.com>
2021-11-10 09:54:59 +0800

    python自动化测试学习

---
+++ API接口测试/day11/jenkins学习.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2021/11/9 18:20
+# @Software       : PythonFullStack
+# @Python_verison : 3.7
---
+++ API接口测试/day11/自动化测试项目用例运行方式.py
@@ -0,0 +1,14 @@
+
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2021/11/9 18:47
+# @Software       : PythonFullStack
+# @Python_verison : 3.7
+
+# 运行自动化测试脚本
+    # 1、我们目前运行自动化用例都是在pycharm中运行
+    # 实际工作中我们的代码一般不在自己电脑上，而是在某个服务器电脑上
+    # 所以，我们一般运行脚本是使用命令行运行
+
+    # 2、通过命令行运行
+    # 切换到run文件目录下python run.py
